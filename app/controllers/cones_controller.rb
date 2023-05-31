b2c76801a197660b271fe47e8dd61329f2877a14
tainah jane <tainah.94@hotmail.com>
2023-05-31 12:16:49 -0400

    added actions for cones files and booking

--- app/controllers/cones_controller.rb
+++ app/controllers/cones_controller.rb
@@ -4,4 +4,26 @@ class ConesController < ApplicationController
     @cones = Cone.all
   end
 
+  def show
+    @cone = Cone.find(params[:id])
+  end
+
+  def new
+    @cone = Cone.new
+  end
+
+  def create
+    @cone = Cone.new(cone_params)
+    if @cone.save
+      redirect_to cone_path(@cone)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cone_params
+    params.require(:cone).permit(:condition, :quantity, :location, :name, :description)
+  end
 end
