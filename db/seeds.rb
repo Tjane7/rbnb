32b83dcc331c7fb89a43e5d4eb52e571823d02d6
Emily Brown <emi.jbrown@gmail.com>
2023-05-31 16:21:11 -0400

    added faker gem and seeds - commented out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# creer utilisateurs
-# creer cone
+# 3.times do
+#   user = User.create!(
+#     email: Faker::Internet.email
+#     password: randomPassword,
+#     # user.password_confirmation: randomPassword,
+#   )
+# end
+
+# 10.times do
+#   cone = Cone.create!(
+#     name: "Cone",
+#     description: Faker::Company.bs,
+#     condition: ["Used", "New"].sample,
+#     quantity: rand(0..15),
+#     location: Faker::Address.city
+#   )
+# end
