f27d63c4226bcb32ee8610c1222c9c1e986e3d88
tainah jane <tainah.94@hotmail.com>
2023-05-31 14:53:50 -0400

    create controller and view for booking

--- app/models/cone.rb
+++ app/models/cone.rb
@@ -2,5 +2,5 @@ class Cone < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :condition, :quantity, :location, :name, :description, presence: true
+  # validates :condition, :quantity, :location, :name, :description, presence: true
 end
