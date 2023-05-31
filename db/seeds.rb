e3740b5c874bc7788872fd1769c0c7de9ae8e0b6
Emily Brown <emi.jbrown@gmail.com>
2023-05-31 17:09:49 -0400

    finished seeding the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,25 @@
-# 3.times do
-#   user = User.create!(
-#     email: Faker::Internet.email
-#     password: randomPassword,
-#     # user.password_confirmation: randomPassword,
-#   )
-# end
+puts "Cleaning the db..."
+Cone.destroy_all
+User.destroy_all
 
-# 10.times do
-#   cone = Cone.create!(
-#     name: "Cone",
-#     description: Faker::Company.bs,
-#     condition: ["Used", "New"].sample,
-#     quantity: rand(0..15),
-#     location: Faker::Address.city
-#   )
-# end
+
+3.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: 111111
+  )
+  puts "Created new user with id #{user.id}"
+end
+
+10.times do
+  cone = Cone.create!(
+    user: User.all.sample,
+    description: Faker::Company.bs,
+    condition: ["Used", "New"].sample,
+    quantity: rand(0..15),
+    location: Faker::Address.city
+  )
+  puts "Created new cone with id #{cone.id}"
+end
+
+puts "Ready!"
