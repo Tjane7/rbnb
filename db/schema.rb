600d93bb4fbabacfe4dd58806407db1e5d35c00d
Emily Brown <emi.jbrown@gmail.com>
2023-05-31 12:20:27 -0400

    added name and description to cones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_155343) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_160026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_155343) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
     t.index ["user_id"], name: "index_cones_on_user_id"
   end
 
