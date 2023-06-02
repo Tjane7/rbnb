e3e5f581f4af306b0cb86b44b94d64e1404724ec
Emily Brown <emi.jbrown@gmail.com>
2023-06-02 13:39:35 -0400

    creating cone seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,107 +30,62 @@ puts "Created new booking user"
 
 # Cone 1
 Cone.create!(
-      user: User.all.sample,
-      name: "Cone orange de New Jersey",
-      description: "This cone is authentic, bright orange, and super lovely.",
-      condition: ["Used", "New"].sample,
-      quantity: rand(0..10),
-      price: 50,
-      location: "26 Oxford Terrace, West Orange",
-      photo:
-    )
-    puts "Created new cone with id #{cone.id}"
+  user: User.all.sample,
+  name: "Cone orange de New Jersey",
+  description: "This cone is authentic, bright orange, and super lovely.",
+  condition: "Used",
+  quantity: 1,
+  price: 50,
+  location: "26 Oxford Terrace, West Orange",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Construction_barrel.jpg"
+)
+puts "Created new cone with id #{cone.id}"
+
 
 # Cone 2
 Cone.create!(
   user: User.all.sample,
-  name: "Name here",
-  description: "This cone is authentic, bright orange, and super lovely.",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..10),
-  price: 50,
-  location: "26 Oxford Terrace, West Orange",
-  photo:
+  name: "Set of construction pylons",
+  description: "Fantastic pylons for everyday use",
+  condition: "Lightly used",
+  quantity: 2,
+  price: 40,
+  location: "San Francisco, California",
+  photo: "https://i.cbc.ca/1.3846449.1494173911!/fileImage/httpImage/image.JPG_gen/derivatives/original_780/montreal-construction.JPG"
 )
 puts "Created new cone with id #{cone.id}"
-
 
 # Cone 3
 Cone.create!(
   user: User.all.sample,
-  name: "Name here",
-  description: "This cone is authentic, bright orange, and super lovely.",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..10),
-  price: 50,
-  location: "26 Oxford Terrace, West Orange",
-  photo:
+  name: "Authentic cone orange",
+  description: "This one is for the cone lovers out there",
+  condition: "Ngl pretty beat up",
+  quantity: 1,
+  price: 30,
+  location: "Brussels, Belgium",
+  photo: "https://m1.quebecormedia.com/emp/emp/68558403_49599280b4b4a9-5630-4586-9103-d36b487c0979_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=0&w=0&h=0&width=800"
 )
 puts "Created new cone with id #{cone.id}"
 
+# require "open-uri"
+# cone_user = Cone.create!(
+#   user: User.all.sample,
+#   description: "Conify",
+#   condition: ["Used", "New"].sample,
+#   quantity: rand(0..15),
+#   price: Faker::Commerce.price
+# )
+#   file = URI.open('https://www.trafficsupply.ca/wp-content/uploads/2016/04/16014-300x300.jpg')
+#   cone.photo.attach(io: file, filename: 'cone.png', content_type: 'image/png')
 
-# Cone 4
-Cone.create!(
-  user: User.all.sample,
-  name: "Name here",
-  description: "This cone is authentic, bright orange, and super lovely.",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..10),
-  price: 50,
-  location: "26 Oxford Terrace, West Orange",
-  photo:
-)
-puts "Created new cone with id #{cone.id}"
-
-# Cone 5
-Cone.create!(
-  user: User.all.sample,
-  name: "Name here",
-  description: "This cone is authentic, bright orange, and super lovely.",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..10),
-  price: 50,
-  location: "26 Oxford Terrace, West Orange",
-  photo:
-)
-puts "Created new cone with id #{cone.id}"
-
-# Cone 6
-Cone.create!(
-  user: User.all.sample,
-  name: "Name here",
-  description: "This cone is authentic, bright orange, and super lovely.",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..10),
-  price: 50,
-  location: "26 Oxford Terrace, West Orange",
-  photo:
-)
-puts "Created new cone with id #{cone.id}"
-
-
-
-
-
-require "open-uri"
-cone_user = Cone.create!(
-  user: User.all.sample,
-  description: "Conify",
-  condition: ["Used", "New"].sample,
-  quantity: rand(0..15),
-  price: Faker::Commerce.price
-)
-  file = URI.open('https://www.trafficsupply.ca/wp-content/uploads/2016/04/16014-300x300.jpg')
-  cone.photo.attach(io: file, filename: 'cone.png', content_type: 'image/png')
-
-  puts "Created new cone with id #{cone.id}"
-end
-
+#   puts "Created new cone with id #{cone.id}"
+# end
 
 # ------------------------------
 # BOOKING SEEDS
 
-5.times do
+3.times do
   booking = Booking.create!(
     user: User.all.sample,
     cone: Cone.all.sample,
